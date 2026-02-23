e27a59884c6180e4d17319c36669ab3b899bae45
tanb01 <tanb0101@gmail.com>
2026-02-24 00:56:28 +0100

    Commented out language button

--- components/LanguageMenu.vue
+++ components/LanguageMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <v-menu open-on-hover bottom offset-y max-width="80px">
+    <!-- <v-menu open-on-hover bottom offset-y max-width="80px">
       <template v-slot:activator="{ on, attrs }">
         <v-btn color="transparent" elevation="0" v-bind="attrs" v-on="on" :small="$vuetify.breakpoint.xsOnly">
           <v-icon :small="$vuetify.breakpoint.xsOnly">
@@ -11,12 +11,12 @@
           </v-icon>
         </v-btn>
       </template>
-      <v-list :dense="$vuetify.breakpoint.xsOnly">
-        <v-list-item v-for="(item, index) in  items" :key="index" :to="switchLocalePath(item.locale)" exact>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+<v-list :dense="$vuetify.breakpoint.xsOnly">
+  <v-list-item v-for="(item, index) in  items" :key="index" :to="switchLocalePath(item.locale)" exact>
+    <v-list-item-title>{{ item.title }}</v-list-item-title>
+  </v-list-item>
+</v-list>
+</v-menu> -->
   </div>
 </template>
 
