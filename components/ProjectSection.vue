a25c199b16d55f42782662d7fd2a4d3aa2a97741
tanb01 <tanb0101@gmail.com>
2026-02-25 01:08:26 +0100

    Remove tabs name for missions

--- components/ProjectSection.vue
+++ components/ProjectSection.vue
@@ -1,9 +1,9 @@
 <template>
   <v-card class="projectSection pa-auto ma-auto transparent" flat>
-    <v-tabs v-model="tab" color="white" centered background-color="transparent" class="mb-6">
-      <v-tabs-slider></v-tabs-slider>
-      <v-tab href="#tab-1" class="tab-header text-h6 rounded-l-sm white--text" v-html="$t('projects.firstTab')" />
-      <v-tab href="#tab-2" class="tab-header text-h6 rounded-r-sm white--text" v-html="$t('projects.secondTab')" />
+    <v-tabs v-model="tab" color="transparent" centered background-color="transparent" class="mb-6">
+      <!-- <v-tabs-slider></v-tabs-slider> -->
+      <v-tab href="#tab-1" />
+      <!-- <v-tab href="#tab-2" class="tab-header text-h6 rounded-r-sm white--text" v-html="$t('projects.secondTab')" /> -->
     </v-tabs>
     <v-tabs-items v-model="tab" id="custom-tab-items">
       <v-tab-item key=0 :value="'tab-1'">
@@ -16,7 +16,7 @@
           </vue-masonry-wall>
         </v-container>
       </v-tab-item>
-      <v-tab-item key=1 :value="'tab-2'">
+      <!-- <v-tab-item key=1 :value="'tab-2'">
         <v-container class="fill-height pa-4" justify-center>
           <vue-masonry-wall :items="sortedEngineeringProjects" :options="{ width: 600, padding: 16 }"
             :ssr="{ columns: 3 }">
@@ -25,7 +25,7 @@
             </template>
           </vue-masonry-wall>
         </v-container>
-      </v-tab-item>
+      </v-tab-item> -->
     </v-tabs-items>
   </v-card>
 </template>
